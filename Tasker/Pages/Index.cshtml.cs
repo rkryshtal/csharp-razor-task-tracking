78b0ca55a2c88c60085b35b3f7ff2b4af206c387
Roman Kryshtal <kryshtal.roman@gmail.com>
2021-11-24 22:11:05 +0200

    Displaying Data on the Home Page

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -9,8 +9,16 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+		private readonly ApplicationDbContext _context;
+		public IEnumerable<Task> Tasks { get; set; }
+
+		public IndexModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
         public void OnGet()
         {
+			Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
